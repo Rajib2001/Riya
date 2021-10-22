359806443fb1329b08857a013a568d80849bac75
Rabiya <91591634+Rajib2001@users.noreply.github.com>
2021-10-22 16:51:32 +0530

    Create demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+
+cout<<"Enter a & b "<<endl;
+cin>>a>>b;
+if(a!=5){
+
+ cout<<"25 can not be divisible"<<endl;
+ }
+if (a<10&&b<5){
+
+ cout<<a+b<<" the sum of a+b cannot be greater than 15"<<endl;
+     
+ }
+
+ if(a==4&&b<=10){
+
+
+    cout<<a%b>>endl;
+ }
+
+    return 0;
+}
+
